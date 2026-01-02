6786a4afba029353bf57255e6f9bb195a2540f93
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2026-01-02 21:38:00 +0530

    ok working

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient();
